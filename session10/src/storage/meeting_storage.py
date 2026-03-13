ff8654a0003bfa3c8be85fbdc4141d81f1b288f7
RonaldoGoatSii <172612901+RonaldoGoatSii@users.noreply.github.com>
2026-03-13 22:01:42 +0000

    feat: add meeting_storage

--- session10/src/storage/meeting_storage.py
+++ session10/src/storage/meeting_storage.py
@@ -1,4 +1,4 @@
-from ..models.meeting_models import Meeting
+from src.models.meeting_models import Meeting
 from uuid import UUID
 
-meeeting_storage: dict[UUID, Meeting] = {}
+meeting_storage: dict[UUID, Meeting] = {}
